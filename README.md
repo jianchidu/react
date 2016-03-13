b817b46898b55ca6f27f017c5eba910d23f8c87d
franklinwa <franklinwa@163.com>
2016-03-13 20:48:26 +0800

    1st commit

---
+++ README.md
@@ -0,0 +1,6 @@
+"# react" learn react
+
+Master
+
+npm install --save react react-dom babelify babel-preset-react
+npm run watch
---
+++ src/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by liuwenju on 2016/3/13.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+ReactDOM.render(
+<h1>Hello,jianchidu React world!</h1>,
+    document.getElementById('example')
+)
